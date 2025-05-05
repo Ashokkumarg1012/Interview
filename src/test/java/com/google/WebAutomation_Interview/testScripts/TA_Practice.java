d126a4c68d0d83407a00dd6cc2af951675e1317d
ashok <ashokkumarg1012@gmail.com>
2025-05-06 00:08:00 +0530

    calendar

--- src/test/java/com/google/WebAutomation_Interview/testScripts/TA_Practice.java
+++ src/test/java/com/google/WebAutomation_Interview/testScripts/TA_Practice.java
@@ -1,4 +1,8 @@
+
 package com.google.WebAutomation_Interview.testScripts;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -8,7 +12,7 @@ import org.testng.annotations.Test;
 import com.google.WebAutomation_Interview.utils.Base;
 
 public class TA_Practice extends Base {
-	@Test
+	@Test(priority=1)
 	public void launch()
 	{
 		driver.get("https://testautomationpractice.blogspot.com/");
@@ -16,9 +20,60 @@ public class TA_Practice extends Base {
 		scrollTo(ele);
 		driver.findElement(By.xpath("//p[contains(text(),'Date Picker 1')]//child::input")).click();
 		driver.findElement(By.xpath("//td[contains(@class,'today')]")).click();
-		String selectedDate = driver.findElement(By.xpath("//td[contains(@class,'today')]//child::a")).getText();
-
-		Assert.assertEquals(selectedDate, "4");
+		String selectedDate = driver.findElement(By.xpath("//td[contains(@class,'today')]//child::a")).getDomAttribute("data-date");
+		String actualDay = getCurrentDate();
+		Assert.assertEquals(actualDay,selectedDate);
+	}
+	
+//	@Test(priority=2)
+	public void dateSelection1_Today()
+	{
+		String date = prop.getProperty("date");
+		calendarAction(date);
+	}
+	@Test(priority=3)
+	public void dateSelection1_day() throws InterruptedException
+	{
+		String day = prop.getProperty("day");
+		driver.findElement(By.xpath("//p[contains(text(),'Date Picker 1')]//child::input")).click();
+		Thread.sleep(1000);
+		String currentMonth = driver.findElement(By.xpath("//span[@class='ui-datepicker-month']")).getText();
+		
+		String month = prop.getProperty("month");
+		int monthValue = Integer.parseInt(month);
+		int currentMonthValue=getMonthValue(currentMonth);
+		int count =monthValue - currentMonthValue;
+//		System.out.println("Count: "+count);
+		System.out.println("current month: "+currentMonthValue);
+		System.out.println("month value: "+monthValue);
+		
+		String year = prop.getProperty("year");
+		int yearValue = Integer.parseInt(year);
+		String currentYear = driver.findElement(By.xpath("//span[@class='ui-datepicker-year']")).getText();
+		int currentYearValue = Integer.parseInt(currentYear);
+		int yearCount = yearValue - currentYearValue;
+		
+		count = count+ 12*yearCount;
+		System.out.println("Count: "+count);
+		for(int m=0;m<count;m++)
+		{
+			driver.findElement(By.xpath("//a[@title='Next']")).click();
+			Thread.sleep(500);
+		}
+		
+		
+		
+		List<WebElement> Calendar_days = driver.findElements(By.xpath("//a[@class='ui-state-default']")); 
+		for(WebElement i:Calendar_days)
+		{
+			if(i.getDomAttribute("data-date").equalsIgnoreCase(day))
+			{
+				Thread.sleep(4000);
+				i.click();
+				break;
+			}
+		}
+		
 	}
 
 }
--- src/test/java/com/google/WebAutomation_Interview/utils/Base.java
+++ src/test/java/com/google/WebAutomation_Interview/utils/Base.java
@@ -2,8 +2,11 @@ package com.google.WebAutomation_Interview.utils;
 
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
@@ -13,6 +16,7 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -24,15 +28,86 @@ import org.testng.annotations.Test;
 
 public class Base {
 	public WebDriver driver;
+	public static Properties prop = new Properties();
 	
 	@BeforeClass
-	public void setup()
+	public void setup() throws IOException
 	{
-		System.getProperty("webdriver.chrome.driver","C://chromedriver.exe");
-		driver = new ChromeDriver();
+		
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//test//java//resources//data.properties");
+		prop.load(fis);
+		if(prop.getProperty("browser").equalsIgnoreCase("chrome"))
+		{
+			System.getProperty("webdriver.chrome.driver","C://chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+		else if(prop.getProperty("browser").equalsIgnoreCase("firefox"))
+		{
+			System.getProperty("webdriver.chrome.driver","C://geckodriver.exe");
+			driver = new FirefoxDriver();	
+		}
+		
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 				
+	}
+	public int getMonthValue(String month)
+	{
+		switch(month)
+		{
+		case "January":
+			return 1;
+			
+		case "February":
+			return 2;
+		
+		case "March":
+			return 3;
+		
+		case "April":
+			return 4;
+			
+		case "May":
+			return 5;
+		
+		case "June":
+			return 6;
+		
+		case "July":
+			return 07;
+		
+		case "August":
+			return 8;
+		
+		case "September":
+			return 9;
+		
+		case "October":
+			return 10;
+			
+		case "November":
+			return 11;
+			
+		case "December":
+			return 12;
+		
+			
+		default:
+			return 00;
+			
+		}
+	}
+	public String getCurrentDate()
+	{
+		 LocalDate currentDate = LocalDate.now();
+		 String date=currentDate.toString();
+		 System.out.println("Current date: "+date);
+		 String subString = date.substring(date.length()-2);
+		 if(subString.charAt(0)=='0')
+		 {
+			 return date.substring(date.length()-1);
+		 }
+		 return subString;
 	}
 	public void scrollTo(WebElement ele)
 	{
@@ -44,6 +119,15 @@ public class Base {
 		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
 		File dest = new File(System.getProperty("user.dir")+"//reports//img.png");
 		FileUtils.copyFile(src, dest);		
+		
+	}
+	public void calendarAction(String date)
+	{
+		String[] str = date.split("/");
+		for(String i:str)
+		{
+			System.out.println(i+" ");
+		}
 		
 	}
 	public void explicitWaitClick(String eleStr)
